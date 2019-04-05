8da714206b92e442e82e2a5ffa2b5189530a1d11
Lenny Pinskiy <lpinskiy@transperfect.com>
2019-04-05 17:07:10 -0400

    got grid to render

--- src/components/Grid.vue
+++ src/components/Grid.vue
@@ -1,20 +1,25 @@
 <template>
   <div class="grid-container">
     <div class="grid-header">
-      <div v-for="(header, idx) in gridSchema" :key="idx">
+      <div v-for="(header, idx) in gridSchema" :key="idx"
+        :style="{'width': `${header.width}%`}">
         {{header.field}}
       </div>
     </div>
     <div class="grid-body">
-      <div v-for="(data, idx) in gridData" class="grid-row" :key="idx">
-         {{data.column1}}
-         {{data.column2}}
-         {{data.column3}}
-         {{data.column4}}
-         {{data.column5}}
-         {{data.column6}}
-         {{data.column7}}
-         {{data.column8}}
+      <div v-for="(data, idx) in gridData" 
+        class="grid-row" 
+        :key="idx">
+         <span :style="{'width': getDataFieldWidth(0)}">{{data.column1}}</span>
+         <span :style="{'width': getDataFieldWidth(1)}">{{data.column2}}</span>
+         <span :style="{'width': getDataFieldWidth(2)}">{{data.column3}}</span>
+         <span :style="{'width': getDataFieldWidth(3)}">{{data.column4}}</span>
+         <span :style="{'width': getDataFieldWidth(4)}">{{data.column5}}</span>
+         <span :style="{'width': getDataFieldWidth(5)}">{{data.column6}}</span>
+         <span :style="{'width': getDataFieldWidth(6)}">{{data.column7}}</span>
+         <span :style="{'width': getDataFieldWidth(7)}">{{data.column8}}</span>
+         <span :style="{'width': getDataFieldWidth(8)}">{{data.column9}}</span>
+         <span :style="{'width': getDataFieldWidth(9)}">{{data.column10}}</span>
       </div>
     </div>
   </div>
@@ -27,10 +32,21 @@ export default {
   props: {
     gridSchema: { type: Array, required: true}, 
     gridData: { type: Array, required: true}
+  },
+  methods: {
+    getDataFieldWidth(idx) {
+      return `${this.gridSchema[idx].width}%`;
+    }
   } 
 }
 
 </script>
 
-<style>
+<style lang="scss">
+
+.grid-container {
+  .grid-row, .grid-header {
+    display: flex;
+  }
+}
 </style>
