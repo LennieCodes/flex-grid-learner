1070802ee2a398037f823202c5a68d8b6052ef45
Lenny Pinskiy <lpinskiy@transperfect.com>
2019-04-05 16:17:23 -0400

    initial commit

--- src/App.vue
+++ src/App.vue
@@ -1,17 +1,24 @@
 <template>
   <div id="app">
-    <img alt="Vue logo" src="./assets/logo.png">
-    <HelloWorld msg="Welcome to Your Vue.js App"/>
+    <Grid :grid-data="gridData" :grid-schema="gridSchema" /> 
   </div>
 </template>
 
 <script>
-import HelloWorld from './components/HelloWorld.vue'
+import Grid from '@/components/Grid';
+import gridData from '@/json/gridData.json';
+import gridSchema from '@/json/gridSchema.json';
 
 export default {
   name: 'app',
   components: {
-    HelloWorld
+    Grid,
+  },
+  data() {
+    return {
+      gridData,
+      gridSchema,
+    }
   }
 }
 </script>
---
+++ src/components/Grid.vue
@@ -0,0 +1,36 @@
+<template>
+  <div class="grid-container">
+    <div class="grid-header">
+      <div v-for="(header, idx) in gridSchema" :key="idx">
+        {{header.field}}
+      </div>
+    </div>
+    <div class="grid-body">
+      <div v-for="(data, idx) in gridData" class="grid-row" :key="idx">
+         {{data.column1}}
+         {{data.column2}}
+         {{data.column3}}
+         {{data.column4}}
+         {{data.column5}}
+         {{data.column6}}
+         {{data.column7}}
+         {{data.column8}}
+      </div>
+    </div>
+  </div>
+</template>
+
+<script>
+
+export default {
+  name: 'grid',
+  props: {
+    gridSchema: { type: Array, required: true}, 
+    gridData: { type: Array, required: true}
+  } 
+}
+
+</script>
+
+<style>
+</style>
